08c03f6234de9cf44fc95c465d5ef3cd3175d769
LesQuatreY <tanguy.minot@laposte.net>
2023-09-30 15:34:12 +0200

    ajout de config.py pour l'interface

---
+++ config.py
@@ -0,0 +1,29 @@
+# Fond orange sur la page principale
+main_page_background = """
+[data-testid="stAppViewContainer"]{
+    background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaQLH29dRw9kQpyvPUJJRfHy6PGk-G-ZqwVDWKjz2HeFAJmDnaBsvq1TjvPal01rTvmUs&usqp=CAU");
+    background-size: cover;
+}
+[data-testid="stHeader"]{
+    background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaQLH29dRw9kQpyvPUJJRfHy6PGk-G-ZqwVDWKjz2HeFAJmDnaBsvq1TjvPal01rTvmUs&usqp=CAU");
+    background-size: cover;
+}
+"""
+
+# Fond avec des haltères sur la sidebar
+sidebar_background = """
+[data-testid="stSidebar"]{
+    background-image: url("https://i.pinimg.com/originals/de/e6/da/dee6da39b926de9739c11409920a353b.jpg");
+    background-size: 165%;
+    background-position: 48% 52%;
+    background-attachment: local;
+    background-color: #FD7605;
+}
+"""
+
+# Fond du message dans la sidebar
+sidebar_message_success = """
+[class="stAlert"]{
+    background-color: #FFFFFF;
+}
+"""
